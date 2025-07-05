067a09d9bd5136fc1ecd5e368b4ac5c50d792ab0
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-07-05 20:02:54 +0530

    Settings: Disable touch polling toggle with battery saver
    Change-Id: I79134af1f57b654602f166d40b140df178a0ecb6

--- src/com/android/settings/display/TouchPollingPreferenceController.java
+++ src/com/android/settings/display/TouchPollingPreferenceController.java
@@ -16,22 +16,70 @@
 
 package com.android.settings.display;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.provider.Settings;
 
-import com.android.settings.core.TogglePreferenceController;
-import com.android.settings.R;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
-public class TouchPollingPreferenceController extends TogglePreferenceController {
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+public class TouchPollingPreferenceController extends TogglePreferenceController
+        implements LifecycleObserver, OnStart, OnStop {
 
     // Settings can only set the debug.* property, so we need to persist it
     // in system settings. Match the stock setting name for backup compatibility.
     private static final String SETTINGS_KEY = "touch_polling_enabled";
     private static final String PROP_NAME = "debug.touch_polling_mode";
 
+    private Preference mPreference;
+    private final PowerManager mPowerManager;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (mPreference != null)
+                updateState(mPreference);
+        }
+    };
+
     public TouchPollingPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        mPowerManager = context.getSystemService(PowerManager.class);
+    }
+
+    @Override
+    public void onStart() {
+        mContext.registerReceiver(mReceiver,
+                new IntentFilter(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED));
+    }
+
+    @Override
+    public void onStop() {
+        mContext.unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return mContext.getString(mPowerManager.isPowerSaveMode()
+                ? R.string.dark_ui_mode_disabled_summary_dark_theme_on
+                : R.string.touch_polling_summary);
     }
 
     @Override
@@ -52,6 +100,13 @@ public class TouchPollingPreferenceController extends TogglePreferenceController
     public boolean isChecked() {
         // debug prop isn't persistent
         return Settings.Secure.getInt(mContext.getContentResolver(), SETTINGS_KEY, 0) == 1;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        super.updateState(preference);
+        refreshSummary(preference);
+        preference.setEnabled(!mPowerManager.isPowerSaveMode());
     }
 
     @Override
